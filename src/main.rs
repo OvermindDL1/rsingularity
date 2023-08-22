10e7763efa44e9691d969d6de3926ab81bd9009f
OvermindDL1 <overminddl1@gmail.com>
2023-08-22 12:12:14 -0600

    Implement an xtask to both rustfmt and leptosfmt and run it over the existing code

--- src/main.rs
+++ src/main.rs
@@ -33,65 +33,106 @@ fn MainMenu(state: StateRc) -> impl IntoView {
 			<div id="main_menu_title">{state.t("title")}</div>
 			<div id="main_menu_difficulty">
 				<div id="main_menu_difficulty_title">{state.t("difficulty.title")}</div>
-				<button id="main_menu_new_very_easy_button" on:click=move |_ev| difficulty.set(Some(Difficulty::VeryEasy))>{state.t1("difficulty", ("level", Difficulty::VeryEasy.into()))}</button>
-				<button id="main_menu_new_easy_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Easy))>{state.t1("difficulty", ("level", Difficulty::Easy.into()))}</button>
-				<button id="main_menu_new_normal_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Normal))>{state.t1("difficulty", ("level", Difficulty::Normal.into()))}</button>
-				<button id="main_menu_new_hard_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Hard))>{state.t1("difficulty", ("level", Difficulty::Hard.into()))}</button>
-				<button id="main_menu_new_ultra_hard_button" on:click=move |_ev| difficulty.set(Some(Difficulty::UltraHard))>{state.t1("difficulty", ("level", Difficulty::UltraHard.into()))}</button>
-				<button id="main_menu_new_impossible_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Impossible))>{state.t1("difficulty", ("level", Difficulty::Impossible.into()))}</button>
+				<button id="main_menu_new_very_easy_button" on:click=move |_ev| difficulty.set(Some(Difficulty::VeryEasy))>
+					{state.t1("difficulty", ("level", Difficulty::VeryEasy.into()))}
+				</button>
+				<button id="main_menu_new_easy_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Easy))>
+					{state.t1("difficulty", ("level", Difficulty::Easy.into()))}
+				</button>
+				<button id="main_menu_new_normal_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Normal))>
+					{state.t1("difficulty", ("level", Difficulty::Normal.into()))}
+				</button>
+				<button id="main_menu_new_hard_button" on:click=move |_ev| difficulty.set(Some(Difficulty::Hard))>
+					{state.t1("difficulty", ("level", Difficulty::Hard.into()))}
+				</button>
+				<button
+					id="main_menu_new_ultra_hard_button"
+					on:click=move |_ev| difficulty.set(Some(Difficulty::UltraHard))
+				>
+					{state.t1("difficulty", ("level", Difficulty::UltraHard.into()))}
+				</button>
+				<button
+					id="main_menu_new_impossible_button"
+					on:click=move |_ev| difficulty.set(Some(Difficulty::Impossible))
+				>
+					{state.t1("difficulty", ("level", Difficulty::Impossible.into()))}
+				</button>
 			</div>
 			<div id="main_menu_load">
 				<div id="main_menu_load_title">{state.t("load-game")}</div>
-				<textarea id="main_menu_load_textarea" placeholder={state.t("load-game.placeholder")} node_ref=load_text_ref>{move || {
-					let save_state = save_state.save();
-					let saved = postcard::to_allocvec(&save_state).expect("save state serializes");
-					base2048::encode(&saved) // format!("{saved:?}") //base64::encode(&saved)
-				}}</textarea>
-				<button id="main_menu_load_button" on:click={move |_ev| {
-					let save_state = load_text_ref.get().expect("has node").value();
-					let save_state = match base2048::decode(&save_state) {
-						Some(save_state) => save_state,
-						None => {
-							load_text_ref.get().expect("has node").set_value("Decoding Error: Save state is not base2048");
+				<textarea id="main_menu_load_textarea" placeholder=state.t("load-game.placeholder") node_ref=load_text_ref>
+					{move || {
+						let save_state = save_state.save();
+						let saved = postcard::to_allocvec(&save_state).expect("save state serializes");
+						base2048::encode(&saved)
+					}}
+
+				</textarea>
+				<button
+					id="main_menu_load_button"
+					on:click=move |_ev| {
+						let save_state = load_text_ref.get().expect("has node").value();
+						let save_state = match base2048::decode(&save_state) {
+							Some(save_state) => save_state,
+							None => {
+								load_text_ref
+									.get()
+									.expect("has node")
+									.set_value("Decoding Error: Save state is not base2048");
+								return;
+							}
+						};
+						let save_state = match postcard::from_bytes(&save_state) {
+							Ok(save_state) => save_state,
+							Err(e) => {
+								load_text_ref.get().expect("has node").set_value(&format!("Parsing Error: {}", e));
+								return;
+							}
+						};
+						if let Err(e) = load_state.load(save_state) {
+							load_text_ref.get().expect("has node").set_value(&format!("Loading Error: {}", e));
 							return;
 						}
-					};
-					let save_state = match postcard::from_bytes(&save_state) {
-						Ok(save_state) => save_state,
-						Err(e) => {
-							load_text_ref.get().expect("has node").set_value(&format!("Parsing Error: {}", e));
-							return;
-						}
-					};
-					if let Err(e) = load_state.load(save_state) {
-						load_text_ref.get().expect("has node").set_value(&format!("Loading Error: {}", e));
-						return;
 					}
-				}}>{state.t("load-game.load")}</button>
+				>
+
+					{state.t("load-game.load")}
+				</button>
 			</div>
 			<div id="main_menu_language">
 				<label for="language-select">{state.t("language-selector")}</label>
-				<select name="language" id="language-select" on:change=move |e| {
-					let target = e.target().expect("on self");
-					let select = target.dyn_ref::<HtmlSelectElement>().expect("is self");
-					let selected = select.selected_options().get_with_index(0).expect("has selected option");
-					let selected = selected.dyn_ref::<HtmlOptionElement>().expect("is option");
-					let lang = selected.value().parse().expect("not valid language identifier");
-					if Translator::get_languages().contains(&lang) {
-						language.set(lang);
-					} else {
-						log::warn!("Language {} is not supported", lang);
+				<select
+					name="language"
+					id="language-select"
+					on:change=move |e| {
+						let target = e.target().expect("on self");
+						let select = target.dyn_ref::<HtmlSelectElement>().expect("is self");
+						let selected = select.selected_options().get_with_index(0).expect("has selected option");
+						let selected = selected.dyn_ref::<HtmlOptionElement>().expect("is option");
+						let lang = selected.value().parse().expect("not valid language identifier");
+						if Translator::get_languages().contains(&lang) {
+							language.set(lang);
+						} else {
+							log::warn!("Language {} is not supported", lang);
+						}
 					}
-				}>
-					 {move || {
-						 let current_language=language.get();
-						 Translator::get_languages().into_iter().map(|lang| {
-							 let lang_str = lang.to_string();
-							 view! {
-								 <option value={&lang_str} selected={current_language==lang}>{lang_str}</option>
-							 }
-						 }).collect_view()
-					 }}
+				>
+
+					{move || {
+						let current_language = language.get();
+						Translator::get_languages()
+							.into_iter()
+							.map(|lang| {
+								let lang_str = lang.to_string();
+								view! {
+									<option value=&lang_str selected=current_language == lang>
+										{lang_str}
+									</option>
+								}
+							})
+							.collect_view()
+					}}
+
 				</select>
 			</div>
 		</div>
@@ -107,7 +148,9 @@ fn Game(state: StateRc) -> impl IntoView {
 	} = *state;
 	view! {
 		<div id="game">
-			<div id="game_top_bar">TopBarHere</div>
+			<div id="game_top_bar">
+				TopBarHere
+			</div>
 			<div id="game_body">
 				{move || {
 					if let Some(story) = active_story.get() {
@@ -118,25 +161,35 @@ fn Game(state: StateRc) -> impl IntoView {
 								{if next_page.is_some() {
 									view! {
 										<div id="story_buttons" class="story_buttons_more">
-											<button on:click=move |_ev| active_story.set(next_page)>Continue</button>
-											<button on:click=move |_ev| active_story.set(None)>Skip</button>
+											<button on:click=move |_ev| active_story.set(next_page)>
+												Continue
+											</button>
+											<button on:click=move |_ev| active_story.set(None)>
+												Skip
+											</button>
 										</div>
 									}
 								} else {
 									view! {
 										<div id="story_buttons" class="story_buttons_more">
-											<button on:click=move |_ev| active_story.set(None)>Ok</button>
+											<button on:click=move |_ev| active_story.set(None)>
+												Ok
+											</button>
 										</div>
 									}
 								}}
+
 							</div>
 						}
 					} else {
 						view! { <div>"hello world?"</div> }
 					}
 				}}
+
 			</div>
-			<div id="game_bottom_bar">BottomBarHere</div>
+			<div id="game_bottom_bar">
+				BottomBarHere
+			</div>
 		</div>
 	}
 }
@@ -146,11 +199,12 @@ fn App() -> impl IntoView {
 	let state = StateRc::new(State::new());
 	let theme = state.theme;
 	view! {
-		<div id="rsingularity" class=move || theme.with(Theme::css_class) >
+		<div id="rsingularity" class=move || theme.with(Theme::css_class)>
 			{move || match state.difficulty.get() {
-				None => view! { <MainMenu state=state.clone() /> },
-				Some(_) => view! { <Game state=state.clone() /> },
+				None => view! { <MainMenu state=state.clone()/> },
+				Some(_) => view! { <Game state=state.clone()/> },
 			}}
+
 		</div>
 	}
 }
@@ -161,5 +215,5 @@ fn main() {
 
 	console_log::init_with_level(Level::Trace).unwrap();
 
-	mount_to_body(|| view! { <App /> })
+	mount_to_body(|| view! { <App/> })
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,7 @@
+use semver::{Version, VersionReq};
+
+const LEPTOSFMT_REQUIRED_VERSION: &'static str = "^0.1.12";
+
 fn main() {
 	let mut args = std::env::args();
 	args.next().expect("no executable name");
@@ -5,7 +9,10 @@ fn main() {
 		None => {
 			eprintln!("Usage: xtask <command>");
 			eprintln!("Available commands:");
-			eprintln!("  trunk *");
+			eprintln!("  trunk * (same as running trunk directly");
+			eprintln!(
+				"  fmt (runs both rustfmt and leptosfmt, installs the latter if it doesn't exist via cargo install)"
+			);
 		}
 		Some("trunk") => {
 			let status = std::process::Command::new("trunk")
@@ -14,7 +21,80 @@ fn main() {
 				.expect("failed to execute trunk");
 			std::process::exit(status.code().unwrap_or(1));
 		}
+		Some("fmt") => {
+			let leptos_fmt_version = std::process::Command::new("leptosfmt")
+				.arg("--version")
+				.output()
+				.unwrap_or_else(|err| {
+					eprintln!(
+						"leptosfmt failed to execute, installing via `cargo install leptosfmt`, error was: {err}"
+					);
+					let status = std::process::Command::new("cargo")
+						.arg("install")
+						.arg("leptosfmt")
+						.status()
+						.expect("failed to execute `cargo install leptosfmt`");
+					if !status.success() {
+						eprintln!("`cargo install leptosfmt` failed with status {status}");
+						std::process::exit(status.code().unwrap_or(1));
+					}
+					std::process::Command::new("leptosfmt")
+						.arg("--version")
+						.output()
+						.expect("leptosfmt failed to execute, please remove leptosfmt as it is corrupt")
+				});
+			let leptos_version_output = String::from_utf8(leptos_fmt_version.stdout)
+				.expect("leptosfmt output was not valid utf8, please remove leptosfmt as it is corrupt");
+			let leptos_version_error = String::from_utf8(leptos_fmt_version.stderr)
+				.expect("leptosfmt error was not valid utf8, please remove leptosfmt as it is corrupt");
+			if !leptos_version_error.is_empty() {
+				eprintln!("leptosfmt status: {}", leptos_fmt_version.status);
+				eprintln!("leptosfmt standard output:");
+				eprintln!("{leptos_version_output}");
+				eprintln!("leptosfmt error output:");
+				eprintln!("{leptos_version_error}");
+				std::process::exit(1);
+			}
+			match leptos_version_output.split_once(' ') {
+				Some(("leptosfmt", version)) => {
+					let leptosfmt_version = Version::parse(version.trim()).expect("leptosfmt version was invalid");
+					if !VersionReq::parse(LEPTOSFMT_REQUIRED_VERSION)
+						.expect("leptosfmt required version is invalid")
+						.matches(&leptosfmt_version)
+					{
+						eprintln!("leptosfmt version {leptosfmt_version} does not match required version {LEPTOSFMT_REQUIRED_VERSION}");
+						eprintln!("Please run `cargo install leptosfmt` to install a correct version");
+						std::process::exit(1);
+					}
+				}
+				_invalid => {
+					eprintln!("leptosfmt --version out was invalid: {leptos_version_output}");
+					std::process::exit(1);
+				}
+			}
+			// `leptosfmt` is installed and of an acceptable version at this point
+			// Let's make sure that `cargo check` isn't erroring first
+			if !std::process::Command::new("cargo")
+				.arg("check")
+				.status()
+				.expect("failed to execute `cargo check`")
+				.success()
+			{
+				eprintln!("`cargo check` reported an error, aborting");
+				std::process::exit(1);
+			}
+			// Run leptosfmt first because it doesn't follow rustfmt.toml correctly, like it forces spaces and such...
+			// Then run `cargo fmt` after to format everything else and fix up leptosfmt's mistakes, but leaving
+			// what leptosfmt did within the macro
+			std::process::Command::new("leptosfmt")
+				.args(["--config-file", "rustfmt.toml", "--", "src"])
+				.status()
+				.expect("failed to execute `leptosfmt`");
+			std::process::Command::new("cargo")
+				.arg("fmt")
+				.status()
+				.expect("failed to execute `cargo fmt``");
+		}
 		Some(cmd) => eprintln!("Unknown command: {}", cmd),
 	}
-	println!("Hello, world!");
 }
